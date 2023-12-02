5d1ebc68df672f33dddd311e90aa08e86d6d6ece
TBHNhien <118433904+TBHNhien@users.noreply.github.com>
2023-12-02 16:51:36 +0700

    #5 loi load du lieu len

--- shopping-cart/routes/index.js
+++ shopping-cart/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var Product = require('../models/product');
+
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('shop/index', { title: 'Thái Bảo Hạo Nhiên' });
+router.get('/', async function(req, res, next) {
+  try {
+    const docs = await Product.find();
+    const productChuck = [];
+    const chucksize = 3;
+    for (let i = 0; i < docs.length; i += chucksize) {
+      productChuck.push(docs.slice(i, i + chucksize));
+    }
+    res.render('shop/index', { title: 'Online shop', products: productChuck });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
